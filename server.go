3da54e1cfebb38400f66a3cb232e57ebda9e8c86
wangshuang <wangshuang@em3.ai>
2021-09-05 23:20:30 +0800

    构建一个基本的tcp服务器

--- server.go
+++ server.go
@@ -6,21 +6,39 @@ import (
 )
 
 type Server struct {
-	Ip string
+	Ip   string
 	Port int
 }
 
 // 创建
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip: ip,
+		Ip:   ip,
 		Port: port,
 	}
 	return server
 }
 
+// 处理业务
+func (this *Server) Handler(conn net.Conn) {
+	fmt.Println("成功建立连接")
+}
+
 // 启动服务器的接口函数
 func (this *Server) Start() {
 	// 创建一个监听套接字
-	net.Listen()
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+
+	}
+	defer listener.Close()
+	// 创建新套接字, 处理客户端的请求
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue
+		}
+		go this.Handler(conn)
+	}
 }
